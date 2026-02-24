f41661de876ae08b63b8c713e188ba1da9559776
yasir azgar <yasirazgar@gmail.com>
2026-02-24 10:18:46 -0400

    Update reviewdog.sh

--- script/reviewdog.sh
+++ script/reviewdog.sh
@@ -31,7 +31,7 @@ bundle exec rubocop \
   --fail-level info \
   | reviewdog -f="rubocop" \
       -name="rubocop" \
-      -reporter="github-pr-annotations" \
+      -reporter="github-pr-review" \
       -filter-mode="nofilter" \
       -level="error" \
       -fail-level="any" \
