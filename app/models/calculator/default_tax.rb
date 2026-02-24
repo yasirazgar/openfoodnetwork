4d2984962576082dd52561dd4e480ed5a783cad6
yasir azgar <yasirazgar@gmail.com>
2026-02-24 13:47:35 -0400

    Update default_tax.rb

--- app/models/calculator/default_tax.rb
+++ app/models/calculator/default_tax.rb
@@ -24,6 +24,7 @@ module Calculator
     end
 
     def compute_order(order)
+      binding.pry
       # This legacy tax calculation applies to additional taxes only, and is no longer used.
       # In theory it should never be called any more after this has been deployed.
       # If the message below doesn't show up in Bugsnag, we can safely delete this method and all
